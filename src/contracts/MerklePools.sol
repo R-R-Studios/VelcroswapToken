27457e5350a7d46dabb0e16b839df035b409e370
0xean <0xean.eth@gmail.com>
2022-04-02 10:22:45 -0600

    adds needed proxy libs

--- src/contracts/MerklePools.sol
+++ src/contracts/MerklePools.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 import "../libraries/FixedPointMath.sol";
 import "../interfaces/IMintableERC20.sol";
